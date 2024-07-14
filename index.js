bcc079e53406e32ea3af5fe87582f8df2d8acbb1
alvin <alvin.sarisar@student.moringaschool.com>
2024-07-15 00:31:50 +0300

    loaded

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+document.addEventListener('DOMContentLoaded', () => {
+    const p = document.getElementById('text');
+    if (p) {
+      p.textContent = "This is really cool!";
+    }
+  });
+  
